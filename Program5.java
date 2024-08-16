07f410d56675f7e9962129ae306924dcc4402163
quiccklabs05 <adibiswas053@gmail.com>
2024-08-16 09:32:18 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,6 +1,39 @@
 //import stuff here?
 
 //Your code here
+public class Program5 {
+    public static void main(String [] args){
+        int Royale1 = 286;
+        int Royale2 = 9;
+        double realRoyale1 = Royale1;
+        double realRoyale2 = Royale2;
+        double avg1 = (realRoyale1/realRoyale2) * 100;
+        int goodIntavg1 = (int)avg1;
+        System.out.println(goodIntavg1);
+        double realavg1 = avg1; //PUT IN gooIntAvg1
+        System.out.println(real
+        realavg1 = (realavg1) / 100;
+        System.out.println("Royale averaged "+ realavg1);
+        
+        
+        int KoopaKing1 = 412;
+        int KoopaKing2 = 40;
+        double realKoopaKing1 = KoopaKing1;
+        double realKoopaKing2 = KoopaKing2;
+        double avg2 = realKoopaKing1/realKoopaKing2;
+        System.out.println("Koopa King averaged "+avg2);
+        
+    }
+}
+
+//Paste console output below:
+/*
+
+
+*/
+//import stuff here?
+
+//Your code here
 
 //Paste console output below:
 /*
